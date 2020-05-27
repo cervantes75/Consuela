5a1222e1d7423fd2a88e8cb5e14ced52c96f8ce8
cervantes75 <63957656+cervantes75@users.noreply.github.com>
2020-05-27 12:12:29 +0200

    Not working updates :)

---
+++ CONSUELA/CONSUELA/Assets/Scripts/MakeItAppear.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeItAppear : MonoBehaviour
+{
+    public GameObject MyPrefab;
+    public Transform newPosition;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(MyPrefab, newPosition.position, Quaternion.identity);
+        }
+    }
+}
