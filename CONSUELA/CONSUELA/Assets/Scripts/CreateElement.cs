2c4bab74d7e5741441eda996780d70b8daf74b25
cervantes75 <63957656+cervantes75@users.noreply.github.com>
2020-06-07 13:42:10 +0200

    instantiate kinda works (tbc)

--- CONSUELA/CONSUELA/Assets/Scripts/CreateElement.cs
+++ CONSUELA/CONSUELA/Assets/Scripts/CreateElement.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CreateElement : MonoBehaviour
 {
     public GameObject sampleObject;
     public Transform ButtonContainer;
     public bool instantiateInWorldSpace = false;
+
     void Start()
     {
         Vector3 localScale = sampleObject.transform.localScale;
+
     }
     public void AddObject()
     {
         GameObject go = Instantiate(sampleObject, Vector3.zero, Quaternion.identity) as GameObject;
         go.transform.SetParent(ButtonContainer, false);
-        go.transform.position = transform.position;
+        if (go.scene.IsValid())
+        {
+            go.transform.position = transform.position + new Vector3(0, 200, 0);
+        }
+        else
+        {
+            go.transform.position = transform.position;
+        }
+       
+       
+        
     }
 }
---
+++ CONSUELA/CONSUELA/Assets/Scripts/rigidb.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidb : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+}
