db3b517c8b5acd095ab6c1a931205470a82e6cc3
cervantes75 <63957656+cervantes75@users.noreply.github.com>
2020-06-07 11:37:47 +0200

    screen optimalization and some error correction

--- CONSUELA/CONSUELA/Assets/Scripts/CreateElement.cs
+++ CONSUELA/CONSUELA/Assets/Scripts/CreateElement.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateElement : MonoBehaviour
 {
     public GameObject sampleObject;
     public Transform ButtonContainer;
+    public bool instantiateInWorldSpace = false;
     void Start()
     {
         Vector3 localScale = sampleObject.transform.localScale;
@@ -13,7 +15,7 @@ public class CreateElement : MonoBehaviour
     public void AddObject()
     {
         GameObject go = Instantiate(sampleObject, Vector3.zero, Quaternion.identity) as GameObject;
-        go.transform.SetParent(ButtonContainer);
+        go.transform.SetParent(ButtonContainer, false);
         go.transform.position = transform.position;
     }
 }
